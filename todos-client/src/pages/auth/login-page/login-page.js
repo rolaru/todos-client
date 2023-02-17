4800d77c0f53ea2e17067ce1a195e32cee2e0b18
Razvan Olaru <olarurazvan_2000@yahoo.com>
2023-02-18 00:40:41 +0200

    feat(login-page): add authentication form to login page

--- todos-client/src/pages/auth/login-page/login-page.js
+++ todos-client/src/pages/auth/login-page/login-page.js
@@ -1,9 +1,32 @@
+import { Link } from 'react-router-dom';
+
 import PageHeader from './../../../components/page-header/page-header';
+import AuthForm from './../../../components/auth-form/auth-form';
+
+import './login-page.css';
 
 const LoginPage = () => {
+
+  const onFormChange = (value) => {};
+
+  const onFormSubmit = (value) => {};
+
   return (
     <div className="page page--centered login-page">
       <PageHeader title="Welcome back!" subtitle="Login to continue." />
+      <AuthForm
+        classes="login-page__form"
+        submitButtonText="Log In"
+        onChange={onFormChange}
+        onSubmit={onFormSubmit}
+      >
+        <Link
+          className="link login-page__register-link"
+          to="/auth/register"
+        >
+          Don’t have an account? Sign up.
+        </Link>
+      </AuthForm>
     </div>
   );
 };
