5eec68e6b588902d2b80e634124f1fcfdfc1b382
Razvan Olaru <olarurazvan_2000@yahoo.com>
2023-02-18 01:12:52 +0200

    feat(register-page): add auth form to register page

--- todos-client/src/pages/auth/register/register-page.js
+++ todos-client/src/pages/auth/register/register-page.js
@@ -1,9 +1,33 @@
+import { Link } from 'react-router-dom'
+
+import AuthForm from '../../../components/auth-form/auth-form';
 import PageHeader from '../../../components/page-header/page-header';
 
+import './register-page.css';
+
 const RegisterPage = () => {
+
+  const onFormChange = (value) => {};
+
+  const onFormSubmit = (value) => {};
+
   return (
     <div className="page page--centered register-page">
       <PageHeader title="Welcome!" subtitle="Sign up to start using Simpledo today." />
+      <AuthForm
+        classes="register-page__form"
+        isRegisterForm={true}
+        submitButtonText="Sign Up"
+        onChange={onFormChange}
+        onSubmit={onFormSubmit}
+      >
+        <Link
+          className="link register-page__login-link"
+          to="/auth/login"
+        >
+          Do have an account? Sign in.
+        </Link>
+      </AuthForm>
     </div>
   );
 };
