b07ff9d3577620f5ab6a1c87d69acda79781e5f9
Razvan Olaru <olarurazvan_2000@yahoo.com>
2023-02-18 06:06:56 +0200

    feat(todos-list): add updateTodo and deleteTodo as dependencies for useCallback

--- todos-client/src/pages/todos-page/todos-list/todos-list.js
+++ todos-client/src/pages/todos-page/todos-list/todos-list.js
@@ -16,8 +16,8 @@ const TodosList = ({ classes, filteredTodos }) => {
 
   // wrap these methods with useCallback so that we benefit of the full power
   // of memo on the TodoItems and they don't not get rerendered uselessly
-  const onChangeTodo = useCallback((id, isDone) => updateTodo(id, isDone), []);
-  const onDeleteTodo = useCallback((id) => deleteTodo(id), []);
+  const onChangeTodo = useCallback((id, isDone) => updateTodo(id, isDone), [updateTodo]);
+  const onDeleteTodo = useCallback((id) => deleteTodo(id), [deleteTodo]);
 
   return (
     <ul className={'todos-list' + extraClasses}>
